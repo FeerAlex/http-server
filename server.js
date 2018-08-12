7359006497d29233e34f08a673cbaebbb68da443
Feer Aleksandr <feer@retailsuite.io>
2018-08-12 16:30:47 +0300

    fix codestyle

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 let http = require('http');
 let interval = process.argv.slice(2)[0] || 1000;
-let stopTime = process.argv.slice(2)[1] || 1000;
 let server = http.createServer().listen(8080);
 let timerId;
 
 server.on('request', (req, res) => {
-
-  // clearInterval(timerId);
-
   if (!timerId) {
-    timerId = setInterval(function() {
+    timerId = setInterval(() => {
       let date = new Date().toISOString();
       console.log(date);
     }, interval);
