38446f9dd90d786d25325a209042b261927d3d13
Feer Aleksandr <feer@retailsuite.io>
2018-08-19 23:51:46 +0300

    fix request

--- server.js
+++ server.js
@@ -1,24 +1,22 @@
 let http = require('http');
-let interval = process.argv.slice(2)[0] || 1000;
-let stop = process.argv.slice(2)[1] || 1000;
-let timerId = null;
+let interval = process.env.INTERVAL || 1000;
+let stop = process.env.STOP || 5000;
 
-const getDate = () => new Date().toISOString();
+const getDate = () => new Date().toUTCString();
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    if (!timerId) {
-      timerId = setInterval(() => {
-        console.log(getDate());
-      }, interval);
-    } else {
-      setTimeout(() => {
-        clearInterval(timerId);
-        timerId = null;
-        res.end(getDate());
-      }, stop);
-    }
+  if (req.url === '/favicon.ico') {
+    return;
   }
+
+  let timerId = setInterval(() => console.log(getDate()), interval);
+  
+  setTimeout(() => {
+    clearInterval(timerId);
+    timerId = null;
+    console.log(getDate());
+    res.end(getDate());
+  }, stop);
 });
 
 server.listen(3000, (err) => {
